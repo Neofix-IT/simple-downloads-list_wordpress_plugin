8669a9ed5013005b7240a23d07a9a03acd6be7ce
maheini35 <maheini35@me.com>
2022-05-14 12:55:03 +0200

    switch to wp_ajax -> and new backend.

--- simple-downloads-list/tags/1.0.0/admin/sdl.js
+++ simple-downloads-list/tags/1.0.0/admin/sdl.js
@@ -1,9 +1,40 @@
 jQuery(document).ready(function(){ 
-	
-	var plugin_folder = neofix_sdl_folder.pluginsUrl;
   
+  var ajax_url = plugin_ajax_object.ajax_url;
+
+  setupTableEdit();
+
+  function setupTableEdit(){
     jQuery('#example10').Tabledit({
-      url: plugin_folder + "/admin/backend.php",
+      url: ajax_url,
+      neofix_sdl_editmethod: 'post',
+      neofix_sdl_deletemethod: 'post',
+      neofix_sdl_restoremethod: 'post',
+      buttons: {
+        edit: {
+            class: 'btn btn-sm btn-default',
+            html: '<span class="fas fa-edit"></span>',
+            action: 'neofix_sdl_edit'
+        },
+        delete: {
+            class: 'btn btn-sm btn-default',
+            html: '<span class="fas fa-trash-alt"></span>',
+            action: 'neofix_sdl_delete'
+        },
+        save: {
+            class: 'btn btn-sm btn-success',
+            html: 'Save'
+        },
+        restore: {
+            class: 'btn btn-sm btn-warning',
+            html: 'Restore',
+            action: 'neofix_sdl_restore'
+        },
+        confirm: {
+            class: 'btn btn-sm btn-danger',
+            html: 'Confirm'
+        }
+      },
       columns: {
         identifier: [0, 'id'],
         editable: [
@@ -14,38 +45,28 @@ jQuery(document).ready(function(){
 			  ]
       }
     });
+  }
 	
-	
-	
-    jQuery("#addRow10").on('click', function () {
-
-      jQuery.ajax({
-        type: "POST",
-        url: plugin_folder + "/admin/add_backend.php",
-        datatype: 'html',
-        success: function (data) {
+  jQuery("#addRow10").on('click', function () {
     
-          // Add 'html' data to table
-          jQuery('#example10 tbody').html(data);
+    var data = {'action': 'neofix_sdl_add'};
 
+    jQuery.ajax({
+      url: ajax_url,
+      type: 'post',
+      data: data,
+      dataType: 'html',
+      success: function (data) {
+  
+        // Add 'html' data to table
+        jQuery('#example10 tbody').html(data);
 
-          jQuery('#example10').Tabledit({
-            url: plugin_folder + "/admin/backend.php",
-            columns: {
-              identifier: [0, 'id'],
-              editable: [
-				  [1, 'name'],
-				  [2, 'description', 'textarea'],
-				  [3, 'category'],
-				  [4, 'download']
-			  ]
-            }
-          });
-        },
-        error: function () {
-          console.log(data);
-    
-        }
-      })
+        setupTableEdit();
+      },
+      error: function () {
+        console.log(data);
+  
+      }
     });
+  });
 }); 
