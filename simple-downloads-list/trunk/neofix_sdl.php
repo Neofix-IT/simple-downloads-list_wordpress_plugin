b0e3afabf9dda83e7af3642699ad8f906711ab9d
maheini35 <maheini35@me.com>
2022-05-25 11:45:49 +0200

    new: trunk

---
+++ simple-downloads-list/trunk/neofix_sdl.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Plugin Name: Simple Downloads List
+ * Plugin URI: http://neofix.ch/
+ * Description: Create a downloads list - quick and easy.
+ * Version: 1.1.0
+ * Author: Martin Heini
+ * Author URI: http://neofix.ch
+ */
+
+require_once "admin/adminpanel.php";
+
+//
+// Admin section
+//
+add_action('admin_menu','neofix_sdl_add_add_menu_page');
+function neofix_sdl_add_add_menu_page()
+{
+    add_menu_page( 'Simple Download List', 'Simple downloads list', 'manage_options', 'neofix_sdl_render_admin_panel', 'neofix_sdl_render_admin_panel');
+}
+
+// 
+// Setup and success message
+//
+register_activation_hook( __FILE__, 'neofix_sdl_admin_notice_activation_hook' );
+function neofix_sdl_admin_notice_activation_hook() {
+    set_transient( 'neofix_sdl_admin_notice', true, 5 );
+}
+
+add_action( 'admin_notices', 'neofix_sdl_admin_notice_show' );
+function neofix_sdl_admin_notice_show(){
+
+    /* Check transient, if available display notice */
+    if( get_transient( 'neofix_sdl_admin_notice' ) ){
+        ?>
+        <div class="updated notice is-dismissible">
+            <p>Thank you for using Simple Downloads List! <strong>You are awesome</strong>.</p>
+        </div>
+        <?php
+        /* Delete transient, only display this notice once. */
+        delete_transient( 'neofix_sdl_admin_notice' );
+    }
+}
+
+// Setup DB
+register_activation_hook( __FILE__, 'neofix_sdl_setup_db' );
+function neofix_sdl_setup_db() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'neofix_sdl';
+	$charset_collate = $wpdb->get_charset_collate();
+	
+	$sql = "CREATE TABLE ".$table_name." (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `name` varchar(250) DEFAULT NULL,
+        `description` text DEFAULT NULL,
+        `category` varchar(250) DEFAULT NULL,
+        `download` text DEFAULT NULL,
+        `deleted` tinyint(1) DEFAULT 0,
+        PRIMARY KEY (`id`)
+    ) $charset_collate;";
+
+	require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+	dbDelta( $sql );
+}
+
+// 
+// Uninstall code
+// 
+register_uninstall_hook(__FILE__, 'neofix_sdl_teardown_db');
+function neofix_sdl_teardown_db(){
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'neofix_sdl';
+    
+    $wpdb->query("DROP TABLE IF EXISTS $table_name");
+}
+
+
+//
+// Add shortcode
+//
+require_once 'lists/download_list_1.php';
+add_shortcode('neofix_sdl', 'neofix_sdl_render_list_1');
+
+?>
---
+++ simple-downloads-list/trunk/readme.txt
@@ -0,0 +1,61 @@
+=== Simple Downloads List ===
+Contributors: neofix
+Donate link: https://neofix.ch
+Tags: download, list, simple, downloads, table, files, attachments
+Requires at least: 4.7
+Tested up to: 6.0
+Requires PHP: 7.0
+Stable tag: 1.1.0
+License: MIT
+License URI: https://github.com/Neofix-IT/simple-downloads-list_wordpress_plugin/blob/main/LICENSE
+
+Create a downloads list - quick and easy. With categories and mobile friendly design
+
+== Description ==
+
+Simple Downloads List allows you to provide multiple downloads as a beautiful list for your visitors. Simply add your downloads in the admin section and insert the shortcode.
+
+= Main features: =
+
+* Fully free - there's no pro version
+* Mobile friendly list design
+* A search bar allows your visitors to search for downloads
+* Show only certain downloads per list by filtering then via categories
+* Multilingual lists: "en" and "de" languages supported
+
+= How to use =
+
+Simply add this shortcode:
+
+`[neofix_sdl language="en" category="category_name"]`
+
+language: Frontend language -> currently "en" (english) and "de" (german) are supported
+category: Which categories should be displayed? If this option is missing, all downloads are visible
+
+== Installation ==
+
+Download and install \"Simple downloads list\" from Wordpress plugins. Add Downloads inside of the admin menu. For the front-end, use this code:
+
+= How to use =
+
+Simply add this shortcode:
+
+`[neofix_sdl language="en" category="category_name"]`
+
+language: Frontend language -> currently "en" (english) and "de" (german) are supported
+category: Which categories should be displayed? If this option is missing, all downloads are visible
+
+== Screenshots ==
+1. Frontend desktop
+2. Frontend mobile
+3. Admin panel
+
+== Changelog ==
+
+= v 1.1.0 =
+
+Tested with Wordpress 6.0 and minor optimisations.
+
+= v 1.0.0 =
+
+Release
