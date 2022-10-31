8455234b3806572e5121c25076111a2d6271125e
maheini35 <maheini35@me.com>
2022-10-31 23:50:08 +0100

    changing translations and removing old content

--- plugin/lists/download_list_1.php
+++ plugin/lists/download_list_1.php
@@ -5,22 +5,9 @@ function neofix_sdl_render_list_1($attr){
 	$table_name = $wpdb->prefix . 'neofix_sdl';
 
 	$args = shortcode_atts( array(
-		'language' => 'en',
 		'category' => ''
 	), $attr );
-	
-	$name_label;
-	$description_label;
-	$search_label;
-	if( $args["language"] == "de" ){
-		$name_label = "Name";
-		$description_label = "Beschreibung";
-		$search_label = "🔎︎  Suchen...";
-	} else {
-		$name_label = "Name";
-		$description_label = "Description";
-		$search_label = "🔎︎  Search...";
-	}
+
 	
 	wp_enqueue_style( 'neofix_sdl_style_1', plugins_url( '../style/style_1.css' , __FILE__ ));
 
@@ -34,12 +21,12 @@ function neofix_sdl_render_list_1($attr){
 	
 	$data = '
 		<div id="neofix_sdl">
-		<input type="text" id="neofix_sdl_search" onkeyup="myFunction()" placeholder="'.__('Search...', 'neofix_sdl_domain').'">
+		<input type="text" id="neofix_sdl_search" onkeyup="myFunction()" placeholder="'.__('Search...', 'simple-downloads-list').'">
 		<table id="neofix_sdl_table">
 			<thead>
 				<tr>
-				<th scope="col">'.__('Name', 'neofix_sdl_domain').'</th>
-				<th scope="col">'.__('Description', 'neofix_sdl_domain').'</th>
+				<th scope="col">'.__('Name', 'simple-downloads-list').'</th>
+				<th scope="col">'.__('Description', 'simple-downloads-list').'</th>
 				<th scope="col"></th>
 				</tr>
 			</thead>
@@ -48,8 +35,8 @@ function neofix_sdl_render_list_1($attr){
 	foreach($result as $row){
 		$data .= '
 			<tr>
-			<td class="column_1" data-label="'.__('Name', 'neofix_sdl_domain').'">'.(empty($row->name) ? '&nbsp;' : esc_html($row->name)).'</td>
-			<td class="column_2" data-label="'.__('Description', 'neofix_sdl_domain').'">'.(empty($row->description) ? '&nbsp;' : nl2br(esc_html($row->description))).'</td>
+			<td class="column_1" data-label="'.__('Name', 'simple-downloads-list').'">'.(empty($row->name) ? '&nbsp;' : esc_html($row->name)).'</td>
+			<td class="column_2" data-label="'.__('Description', 'simple-downloads-list').'">'.(empty($row->description) ? '&nbsp;' : nl2br(esc_html($row->description))).'</td>
 			<td class="column_3"><a class="sdl_download" href="'.(empty($row->download) ? '#' : esc_html($row->download)).'" download>Download</button></td>
 			</tr>
 			';
