dadb0b12e3f31e08c6e8575e91e4cefa959697b6
Martin Heini <maheini35@me.com>
2024-05-11 13:33:01 +0200

    simplify & modularize rendering process

--- block/src/list_1.php
+++ block/src/list_1.php
@@ -1,5 +1,21 @@
-<div id="neofix_sdl">
-    <input type="text" id="neofix_sdl_search" onkeyup="myFunction()" placeholder="<?php echo __('Search...', 'simple-downloads-list') ?>">
+<?php
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'neofix_sdl';
+
+    $category = esc_sql($category);
+
+    $result;
+    if($category == ""){
+        $result = $wpdb->get_results("SELECT * FROM ".$table_name." WHERE deleted IS FALSE ORDER BY id DESC");
+    } else {
+        $result = $wpdb->get_results("SELECT * FROM ".$table_name." WHERE deleted IS FALSE 
+        AND category = '".$category."' ORDER BY id DESC");
+    }
+
+?>
+
+<div id="neofix_sdl" <?php echo get_block_wrapper_attributes(); ?>>
+    <input type="text" id="neofix_sdl_search" onkeyup="filterSDL()" placeholder="<?php echo __('Search...', 'simple-downloads-list') ?>">
     <table id="neofix_sdl_table">
         <thead>
             <tr>
@@ -14,8 +30,8 @@
             ?>
             <tr>
             <td class="column_1" data-label="<?php echo __('Name', 'simple-downloads-list') ?>"><?php echo (empty($row->name) ? '&nbsp;' : esc_html($row->name)) ?></td>
-            <td class="column_2" data-label="<?php __('Description', 'simple-downloads-list') ?>">'.(empty($row->description) ? '&nbsp;' : nl2br(esc_html($row->description))).'</td>
-            <td class="column_3"><a class="sdl_download" href="'.(empty($row->download) ? '#' : esc_html($row->download)).'" download>Download</button></td>
+            <td class="column_2" data-label="<?php __('Description', 'simple-downloads-list') ?>"><?php echo (empty($row->description) ? '&nbsp;' : nl2br(esc_html($row->description))) ?></td>
+            <td class="column_3"><a class="sdl_download" href="'<?php (empty($row->download) ? '#' : esc_html($row->download)) ?>'" download>Download</button></td>
             </tr>
             <?php
             }
@@ -30,7 +46,7 @@
 </div>
 		
 <script>
-	function myFunction() {
+	function filterSDL() {
 		// Declare variables
 		var input, filter, table, tr, td, i, txtValue;
 		input = document.getElementById("neofix_sdl_search");
