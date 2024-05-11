a27908eb4e10f70a1777299e3dda6852d9b959cb
Martin Heini <maheini35@me.com>
2024-05-11 13:31:21 +0200

    refactored download block php part

--- neofix_sdl.php
+++ neofix_sdl.php
@@ -9,12 +9,16 @@
  * Text Domain: simple-downloads-list
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+    
 define( 'NEOFIX_SDL_PATH', plugin_dir_url( __FILE__ ) );
 define( 'NEOFIX_SDL_PATH_LOCAL', __DIR__ );
 
 require_once "setup/setup.php";
 require_once "admin/adminpanel_v2.php";
 require_once 'lists/list_1/download_list_1.php';
-require_once 'block/neofix-sdl.php';
+require_once 'block/download_block.php';
 
 ?>
