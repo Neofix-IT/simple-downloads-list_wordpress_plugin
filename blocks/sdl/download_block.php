f8c1f18ffc78397b501c0f8d5b0b31ad298dd880
maheini <maheini35@me.com>
2025-11-01 23:52:27 +0100

    refactor: move sdl block to blocks/sdl folder

---
+++ blocks/sdl/download_block.php
@@ -0,0 +1,92 @@
+<?php
+
+if (! defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+
+class SDL_Download_Block
+{
+    function __construct()
+    {
+        add_action('init', array($this, 'register_block'));
+        add_action('rest_api_init', array($this, 'register_api_routes'));
+    }
+
+    /**
+     * Registers the block using the metadata loaded from the `block.json` file.
+     * Behind the scenes, it registers also all assets so they can be enqueued
+     * through the block editor in the corresponding context.
+     *
+     * @see https://developer.wordpress.org/reference/functions/register_block_type/
+     */
+    function register_block()
+    {
+        register_block_type(__DIR__);
+    }
+
+    /**
+     * Registers alle the neccessary api routes required fot the download block
+     */
+    function register_api_routes()
+    {
+        register_rest_route(
+            'neofix-sdl/v1',
+            '/editor-preview/',
+            array(
+                'methods' => 'GET',
+                'callback' => array($this, 'sdl_preview'),
+                'permission_callback' => array($this, 'has_admin_privilege')
+            )
+        );
+        register_rest_route(
+            'neofix-sdl/v1',
+            '/download-categories/',
+            array(
+                'methods' => 'GET',
+                'callback' => array($this, 'get_download_categories'),
+                'permission_callback' => array($this, 'has_admin_privilege')
+            )
+        );
+    }
+
+    function has_admin_privilege()
+    {
+        return current_user_can('administrator');
+    }
+
+    /** 
+     * Retrieves distinct download categories from WP-DB and returns an array
+     * 
+     * @return array
+     */
+    function get_download_categories()
+    {
+        global $wpdb;
+        $table_name = $wpdb->prefix . "neofix_sdl";
+
+        $result = $wpdb->get_results("SELECT DISTINCT `category` FROM `$table_name` WHERE `category` IS NOT NULL AND `category` <> '' ORDER BY `category` DESC");
+        $categories = array();
+
+        foreach ($result as $category) {
+            $categories[] = esc_html($category->category);
+        }
+        return $categories;
+    }
+
+    /**
+     * Generates a preview of the list based on the url parameter `category`
+     * 
+     * @param object $data	the request object
+     */
+    function sdl_preview($data)
+    {
+        $category = esc_sql($data->get_param('category')) ?? "";
+
+        ob_start();
+        include __DIR__ . '/list_1.php';
+        return ob_get_clean();
+    }
+}
+
+new SDL_Download_Block();
---
+++ blocks/sdl/list_1.php
@@ -0,0 +1,70 @@
+<?php
+global $wpdb;
+$table_name = $wpdb->prefix . 'neofix_sdl';
+
+if ($category == "") {
+    $query = "SELECT * FROM `$table_name` WHERE deleted IS false  ORDER BY id DESC";
+} else {
+    $query = $wpdb->prepare("SELECT * FROM `$table_name` WHERE deleted IS false AND category=%s ORDER BY id DESC", $category);
+}
+
+$result = $wpdb->get_results($query);
+?>
+
+<div id="neofix_sdl" <?php echo get_block_wrapper_attributes(); ?>>
+    <input type="text" id="neofix_sdl_search" onkeyup="filterSDL()" placeholder="<?php echo __('Search...', 'simple-downloads-list') ?>">
+    <table id="neofix_sdl_table">
+        <thead>
+            <tr>
+                <th scope="col"><?php echo __('Name', 'simple-downloads-list') ?></th>
+                <th scope="col"><?php echo __('Description', 'simple-downloads-list') ?></th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($result as $row) {
+            ?>
+                <tr>
+                    <td class="column_1" data-label="<?php echo __('Name', 'simple-downloads-list') ?>"><?php echo (empty($row->name) ? '&nbsp;' : esc_html($row->name)) ?></td>
+                    <td class="column_2" data-label="<?php __('Description', 'simple-downloads-list') ?>"><?php echo (empty($row->description) ? '&nbsp;' : nl2br(esc_html($row->description))) ?></td>
+                    <td class="column_3"><a class="sdl_download" href="'<?php (empty($row->download) ? '#' : esc_html($row->download)) ?>'" download>Download</button></td>
+                </tr>
+            <?php
+            }
+            if (count($result) == 0) {
+            ?>
+                <tr>
+                    <td colspan="3" class="no-data" style="text-align: center;">
+                        <p><?php echo __('No downloads available', 'simple-downloads-list') ?></p>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
+
+<script>
+    function filterSDL() {
+        // Declare variables
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("neofix_sdl_search");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("neofix_sdl_table");
+        tr = table.getElementsByTagName("tr");
+
+        // Loop through all table rows, and hide those who dont match the search query
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
+    setRowColor();
+</script>
